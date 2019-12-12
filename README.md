f104d6539e0950eca10d98ff97947030c6aff7a3
Apryl S <aprylsusi@gmail.com>
2019-12-12 18:23:04 -0500

    Fifth change

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-#Apryl's Sad Attempt at a Final Project#
-##Python judges your favorite things##
-###It's too much of an effort to try to run a data analysis project at this point without data readily available###
+#Apryl's Sad Attempt at a Final Project
+
+##Python judges your favorite things
 
 File originally created 12/5/19
-Updated 12/11/19 to exclude non-character strings
+
+Last updated 12/12/19
+
+
+This package in Python is designed to give you feedback on your favorite animals.
+
+This package incorporates user defined functions, nested if then else statements, the inbuilt type and length functions, as well as return statements. It incorpoarates a check to make sure that a character value is being entered.
+
+Future fixes and add ons for this package would be to correctly identify character/string versions of numbers, add in another argument to be fed into the function, which item you want judged (i.e. color, food, team, etc.), and add in a random aspect to the feedback received.
 
--- apryls_final_project/sad_attempt.py
+++ apryls_final_project/sad_attempt.py
@@ -1,46 +1,8 @@
-#Tell python your favorite animal and get an opinion and your lottery numbers
-def judgy_python (animal):
-    name_length = 0    
-    for i in animal: 
-        name_length += 1    
-    If 0 <= name_length <=1: 
-        return "Are you even trying?"
-    Elif 2 <= name_length <= 3:
-        return "Embarassing choice"
-    Elif 4 <= name_length <= 6:
-        return "Interesting..."
-    Elif 7 <= name_length <= 10:
-        return "Very original"
-    Elif 11 <= name_length <= 15:
-        return "I guess that's cool"
-    Else:
-        return "That's not even real"
-
-judgy_python("bat")
-
-
-
-def judgy_python (animal):
-    name_len = len(animal)    
-    if name_len <= 1: 
-        response="Are you even trying?"
-    elif name_len <= 3:
-        response="Embarassing choice"
-    elif name_len <= 6:
-        response="Interesting..."
-    elif name_len <= 10:
-        response="Very original"
-    elif name_len <= 15:
-        response="I guess that's cool"
-    else:
-        response="That's not even real"
-    return response
-judgy_python("bat")
-judgy_python("23")
-
-
+#User defined function incorporating 
 #12/11/19
 def judgy_python (animal):
+    """Tell Python what your favorite animal is and it will give you excellant feedback on your choice.
+        First created on 12/5/2019."""
     name_type= type(animal)
     if name_type==str:
         name_len = len(animal)
